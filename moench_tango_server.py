2afa0395b70905773ee03a40dec7be07b96db4f2
lrlunin <lunin@campus.tu-berlin.de>
2021-12-12 16:44:25 +0100

    add some attributes

--- moench_tango_server.py
+++ moench_tango_server.py
@@ -7,6 +7,16 @@ import os
 import signal
 
 class MoenchDetector(Device):
+	exposure = attribute(label = "exposure [in seconds]", dtype = "float",
+                   access = AttrWriteType.READ_WRITE, memorized = True,
+                   hw_memorized = True, polling_period = polling)
+    trigger_mode = attribute(label = "trigger mode [auto/external trigger]")
+    filename = attribute(label = "file name for output data file", dtype = "str")
+    filepath = attribute(label = "dir where data files will be written", dtype = "str")
+    frames = attribute(label = "number of frames per acquisition", dtype = "int")
+    filewrite = attribute(label = "enable or disable file writing", dtype = "bool")
+    highvoltage = attribute(label = "high voltage on sensor [60-200V]", dtype = "int")
+        
 	def init_pc(self):
 		SLS_RECEIVER_PORT = "1954"
 		PROCESSING_RX_IP_PORT = "192.168.2.200 50003"
@@ -15,8 +25,8 @@ class MoenchDetector(Device):
 		CONFIG_PATH = "/home/moench/detector/moench_2021_virtual.config" #for virtual detector
 		#CONFIG_PATH = "/home/moench/detector/moench_2021.config" #for real detector
 		#configured for moench pc only
-		self.slsDetectorProc = subprocess.Popen("exec slsReceiver -t {}".format(SLS_RECEIVER_PORT), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn = os.setsid)
-		self.zmqDataProc = subprocess.Popen("exec moench04ZmqProcess {} {} {}".format(PROCESSING_RX_IP_PORT, PROCESSING_TX_IP_PORT, PROCESSING_CORES), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn = os.setsid)
+		self.slsDetectorProc = subprocess.Popen("exec slsReceiver -t {}".format(SLS_RECEIVER_PORT), shell=True, stdout=subprocess.PIPE, stderr = subprocess.PIPE, preexec_fn = os.setsid)
+		self.zmqDataProc = subprocess.Popen("exec moench04ZmqProcess {} {} {}".format(PROCESSING_RX_IP_PORT, PROCESSING_TX_IP_PORT, PROCESSING_CORES), shell = True, stdout = subprocess.PIPE, stderr =s ubprocess.PIPE, preexec_fn = os.setsid)
 		self.put_config = subprocess.Popen("exec sls_detector_put config {}".format(CONFIG_PATH), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		sls_running = self.slsDetectorProc.poll() == None
 		zmq_running = self.zmqDataProc.poll() == None
@@ -46,6 +56,8 @@ class MoenchDetector(Device):
 			print("SlsReceiver or zmq socket processes were killed.")
 		except Exception:
 			print("Unnable to kill slsReceiver or zmq socket. Please kill it manually.")
+	
+	def exptime(self):
 		
 if __name__ == "__main__":
 	MoenchDetector.run_server()
