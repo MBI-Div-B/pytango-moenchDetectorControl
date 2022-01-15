0775d2d0a671a784167b7f6e1954a85a46b412a2
lrlunin <lunin@campus.tu-berlin.de>
2022-01-15 13:52:09 +0100

    fixed run command for virtual

--- start_tango_servers.sh
+++ start_tango_servers.sh
@@ -7,7 +7,7 @@ then
 else
   if [[ $* == *--virtual* ]]
   then
-    python3 moench_tango_control_server.py moench -ORBendPoint giop:tcp:localhost:1234 -nodb -dlist moench/virtual/control && python3 moench_tango_acquire_server.py moench -ORBendPoint giop:tcp:localhost:1235 -nodb -dlist moench/virtual/acquire && echo ">>>> Virtual MOENCH tango server enviroment started"
+    python3 moench_tango_control_server.py moench -ORBendPoint giop:tcp:localhost:1234 -nodb -dlist moench/virtual/control --virtual && python3 moench_tango_acquire_server.py moench -ORBendPoint giop:tcp:localhost:1235 -nodb -dlist moench/virtual/acquire && echo ">>>> Virtual MOENCH tango server enviroment started"
   else
   #TODO: need to be configured with proper ip adresses and device names from the tango-DB
   #python3 moench_tango_control_server.py moench -ORBendPoint giop:tcp:localhost:1234 -nodb -dlist moench/virtual/control && python3 moench_tango_acquire_server.py moench -ORBendPoint giop:tcp:localhost:1235 -nodb -dlist moench/virtual/acquire
