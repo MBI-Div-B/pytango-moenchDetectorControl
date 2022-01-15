479e145c344d78f6cb092bec93bdb1e86bc4e603
lrlunin <lunin@campus.tu-berlin.de>
2022-01-15 20:53:11 +0100

    fixed ip

--- computer_setup.py
+++ computer_setup.py
@@ -9,7 +9,7 @@ from pathlib import PosixPath
 class ComputerSetup:
     def init_pc(self, virtual=False):
         SLS_RECEIVER_PORT = "1954"
-        PROCESSING_RX_IP_PORT = "192.168.3.200 50003"
+        PROCESSING_RX_IP_PORT = "192.168.2.200 50003"
         PROCESSING_TX_IP_PORT = "192.168.1.118 50001"
         PROCESSING_CORES = "20"
         if virtual:
